5eb3fe5c3b6bb923c063e2d8e718ed77367d0135
aurbroszniowski <jsoftbiz@gmail.com>
2014-10-27 23:55:47 +0100

    added remove op

--- src/main/java/org/rainfall/ehcache/operation/RemoveOperation.java
+++ src/main/java/org/rainfall/ehcache/operation/RemoveOperation.java
@@ -1,22 +1,61 @@
 package org.rainfall.ehcache.operation;
 
+import net.sf.ehcache.Ehcache;
 import org.rainfall.AssertionEvaluator;
 import org.rainfall.Configuration;
+import org.rainfall.ObjectGenerator;
 import org.rainfall.Operation;
+import org.rainfall.SequenceGenerator;
 import org.rainfall.TestException;
+import org.rainfall.ehcache.CacheConfig;
+import org.rainfall.ehcache.statistics.EhcacheResult;
+import org.rainfall.statistics.Result;
 import org.rainfall.statistics.StatisticsObserversFactory;
+import org.rainfall.statistics.Task;
 
 import java.util.List;
 import java.util.Map;
 
+import static org.rainfall.ehcache.statistics.EhcacheResult.EXCEPTION;
+import static org.rainfall.ehcache.statistics.EhcacheResult.MISS;
+import static org.rainfall.ehcache.statistics.EhcacheResult.REMOVE;
+
 /**
  * @author Aurelien Broszniowski
  */
-public class RemoveOperation<K, V> extends Operation{
+public class RemoveOperation<K, V> extends Operation {
 
   @Override
   public void exec(final StatisticsObserversFactory statisticsObserversFactory, final Map<Class<? extends Configuration>,
       Configuration> configurations, final List<AssertionEvaluator> assertions) throws TestException {
 
+    CacheConfig<K, V> cacheConfig = (CacheConfig<K, V>)configurations.get(CacheConfig.class);
+    SequenceGenerator sequenceGenerator = cacheConfig.getSequenceGenerator();
+    final long next = sequenceGenerator.next();
+    Double weight = cacheConfig.getRandomizer().nextDouble(next);
+    if (cacheConfig.getOperationWeights().get(weight) == OperationWeight.OPERATION.REMOVE) {
+      List<Ehcache> caches = cacheConfig.getCaches();
+      final ObjectGenerator<K> keyGenerator = cacheConfig.getKeyGenerator();
+      for (final Ehcache cache : caches) {
+        statisticsObserversFactory.getStatisticObserver(cache.getName(), EhcacheResult.values())
+            .measure(new Task() {
+
+              @Override
+              public Result definition() throws Exception {
+                boolean removed;
+                try {
+                  removed = cache.remove(keyGenerator.generate(next));
+                } catch (Exception e) {
+                  return EXCEPTION;
+                }
+                if (removed) {
+                  return REMOVE;
+                } else {
+                  return MISS;
+                }
+              }
+            });
+      }
+    }
   }
 }
